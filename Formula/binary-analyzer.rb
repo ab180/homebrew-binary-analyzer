0cb75bab5a243d7fbaa4d436e2467365327c8680
pokeum <pokeum@ab180.co>
2025-06-20 16:13:08 +0900

    fix code

--- Formula/binary-analyzer.rb
+++ Formula/binary-analyzer.rb
@@ -3,10 +3,10 @@ class BinaryAnalyzer < Formula
   homepage "https://www.airbridge.io/"
 
   version = "0.0.0"
-  sha_macos_aarch64 = "ce66d782b51c53ce1e2976d9044a24dd64321486fd1d8bb447ab61b048b80416"
-  sha_macos_x64 = "141fd7b732fde5093ace6130083202a1d21bb14057d7c53f5be7d737fa215df8"
+  sha_macos_aarch64 = "72a0f59075c35eeaec705f192cb4abca6594231f01846e54aa5d970f0acb4498"
+  sha_macos_x64 = "96f1293f5359c2f22e39107bcf4cf36f8811916f00ef90dcdf94f0f32c319406"
   # sha_linux_aarch64 = ""
-  sha_linux_x64 = "d0c87cd7d74bec3315141f00431face73c672bbd67cfa31231c7b898f18b3924"
+  sha_linux_x64 = "e4fcb32135a385bb9483e045f5584b92d9c8dc5a7147a7f252197a839253218d"
 
   on_macos do
     if Hardware::CPU.arm?
